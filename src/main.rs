20a1d608709c589b8a4229c71e8af70ebc632f7c
Jacob Asper <jacobasper191@gmail.com>
2023-05-23 23:47:57 -0400

    parse secrets

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use anyhow::anyhow;
-use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::prelude::ChannelId;
 use serenity::prelude::*;
+use serenity::{async_trait, model::prelude::GuildId};
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
 
-struct Bot;
+mod secrets;
+
+use secrets::{get_secrets, Secrets};
+
+struct Bot {
+    guild_id: GuildId,
+    channel_id: ChannelId,
+}
 
 #[async_trait]
 impl EventHandler for Bot {
@@ -27,18 +34,20 @@ impl EventHandler for Bot {
 async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
-
     // Set gateway intents, which decides what events the bot will be notified about
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
+    let Secrets {
+        token,
+        channel_id,
+        guild_id,
+    } = get_secrets(secret_store);
+
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+        .event_handler(Bot {
+            guild_id,
+            channel_id,
+        })
         .await
         .expect("Err creating client");
 
---
+++ src/secrets.rs
@@ -0,0 +1,43 @@
+use serenity::model::prelude::{ChannelId, GuildId};
+use shuttle_secrets::SecretStore;
+
+pub struct Secrets {
+    token: String,
+    guild_id: GuildId,
+    channel_id: ChannelId,
+}
+
+/// get secrets from secrets.toml and parse them appropriately
+pub fn get_secrets(secret_store: SecretStore) -> Secrets {
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        panic!("'DISCORD_TOKEN' was not found");
+    };
+
+    let channel_id: ChannelId = if let Some(channel_id) = secret_store.get("DISCORD_CHANNEL_ID") {
+        let channel_id: u64 = match channel_id.parse() {
+            Ok(channel_id) => channel_id,
+            Err(error) => panic!("failed to parse DISCORD_CHANNEL_ID: {error}"),
+        };
+        ChannelId(channel_id)
+    } else {
+        panic!("'DISCORD_CHANNEL_ID' was not found");
+    };
+
+    let guild_id = if let Some(guild_id) = secret_store.get("DISCORD_GUILD_ID") {
+        let guild_id: u64 = match guild_id.parse() {
+            Ok(guild_id) => guild_id,
+            Err(error) => panic!("failed to parse DISCORD_GUILD_ID: {error}"),
+        };
+        GuildId(guild_id)
+    } else {
+        panic!("'DISCORD_GUILD_ID' was not found");
+    };
+
+    Secrets {
+        token,
+        guild_id,
+        channel_id,
+    }
+}
