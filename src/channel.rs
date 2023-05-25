83822d494f703b6fe6b49572f8bb1896aad1837b
Jacob Asper <jacobasper191@gmail.com>
2023-05-25 00:00:15 -0400

    add new_channel_name field to update_channel_name fucntion

--- src/channel.rs
+++ src/channel.rs
@@ -4,8 +4,9 @@ use serenity::model::prelude::{ChannelId, GuildChannel};
 pub async fn update_channel_name(
     ctx: Context,
     channel_id: ChannelId,
+    new_channel_name: &str,
 ) -> Result<GuildChannel, serenity::Error> {
     channel_id
-        .edit(&ctx, |channel| channel.name("cool-name"))
+        .edit(&ctx, |channel| channel.name(new_channel_name))
         .await
 }
--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ struct Bot {
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!hello" {
-            match update_channel_name(ctx, self.channel_id).await {
+            let name = "cheese";
+            match update_channel_name(ctx, self.channel_id, name).await {
                 Ok(_) => println!("update channel name successful!"),
                 Err(error) => panic!("update channel name failed: {error}"),
             };
