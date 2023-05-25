a6f9a37a96eb5477568d605b5fec5c2d50c086cd
Jacob Asper <jacobasper191@gmail.com>
2023-05-25 19:14:47 -0400

    use environment variables instead of from secret store

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,27 +1,24 @@
 use serenity::model::prelude::ChannelId;
-use shuttle_secrets::SecretStore;
+use std::env;
 
 pub struct Secrets {
     pub token: String,
     pub channel_id: ChannelId,
 }
 
-/// get secrets from secrets.toml and parse them appropriately
-pub fn get_secrets(secret_store: SecretStore) -> Secrets {
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        panic!("'DISCORD_TOKEN' was not found");
+/// get secrets from environment and parse them appropriately
+pub fn get_secrets() -> Secrets {
+    let token = match env::var("DISCORD_TOKEN") {
+        Ok(token) => token,
+        Err(error) => panic!("'DISCORD_TOKEN' was not found: {error}"),
     };
 
-    let channel_id: ChannelId = if let Some(channel_id) = secret_store.get("DISCORD_CHANNEL_ID") {
-        let channel_id: u64 = match channel_id.parse() {
+    let channel_id: ChannelId = match env::var("DISCORD_CHANNEL_ID") {
+        Ok(channel_id) => match channel_id.parse() {
             Ok(channel_id) => channel_id,
             Err(error) => panic!("failed to parse DISCORD_CHANNEL_ID: {error}"),
-        };
-        ChannelId(channel_id)
-    } else {
-        panic!("'DISCORD_CHANNEL_ID' was not found");
+        },
+        Err(error) => panic!("'DISCORD_CHANNEL_ID' was not found: {error}"),
     };
 
     Secrets { token, channel_id }
